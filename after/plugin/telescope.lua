c4b6010d6115aa99f5c760a7db4bf23a82f69062
Jelso13 <jameselson19@gmail.com>
2022-12-28 20:12:15 +0000

    Added telescope setup

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,6 +2,42 @@ local builtin = require('telescope.builtin')
 local utils = require("telescope.utils")
 local previewers = require("telescope.previewers")
 
+local telescope = require("telescope")
+
+telescope.setup{
+    defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-h>"] = "which_key"
+      }
+    },
+    prompt_prefix = "🔍",
+    file_previewer = previewers.vim_buffer_cat.new,
+    grep_previewer = previewers.vim_buffer_vimgrep.new,
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  }
+}
+
 -- 'project-files' - find project files
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "find files" })
 
