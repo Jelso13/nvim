186f99e9aca3c20f785a5c45879b6bc11130a6ca
Jelso13 <jameselson19@gmail.com>
2022-12-29 15:53:57 +0000

    Started changing how markdown/obsidian is done

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -1,3 +1,13 @@
+-- #######################################
+-- ###### MAYBE REMOVE THIS ##############
+-- #######################################
+
+
+
+
+
+
+
 local status_ok, obsidian = pcall(require, "obsidian")
 if not status_ok then
     return
@@ -20,26 +30,24 @@ local setup = {
         return out
     end,
     -- function that determines how new notes are named
-    note_id_func = function()
-        -- determine if there is a '/' in the file name for if the file
-        -- should be in a subdirectory
-        -- name = vim.fn.input("File Name: ")
-        return vim.fn.input("File Name: ")
+    -- note_id_func = function(title)
+    --     -- determine if there is a '/' in the file name for if the file
+    --     -- should be in a subdirectory
+    --     -- name = vim.fn.input("File Name: ")
+    --     return vim.fn.input("File Name: ")
+    -- end,
+    note_id_func = function(title)
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        local suffix = ""
+        if title ~= nil then
+            -- If title is given, transform it into valid file name.
+            suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        else
+            -- If title is nil, just add 4 random uppercase letters to the suffix.
+            suffix = vim.fn.input("File Name: ")
+        end
+        return suffix
     end,
---     note_id_func = function(title)
---         -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
---         local suffix = ""
---         if title ~= nil then
---             -- If title is given, transform it into valid file name.
---             suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
---         else
---             -- If title is nil, just add 4 random uppercase letters to the suffix.
---             for _ = 1, 4 do
---                 suffix = suffix .. string.char(math.random(65, 90))
---             end
---         end
---         return tostring(os.time()) .. "-" .. suffix
---     end,
     notes_subdir = ".",
 
 }
@@ -47,7 +55,8 @@ local setup = {
 -- render?
 vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianOpen<cr>", {desc="Open note"})
 vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<cr>", {desc="Create new note"})
-vim.keymap.set("v", "<leader>ol", "<cmd>ObsidianLink<cr>", {desc="link note"})
+vim.keymap.set("v", "<leader>ol", "<cmd>ObsidianLinkNew<cr>", {desc="link new note"})
+vim.keymap.set("v", "<leader>oc", "<cmd>ObsidianCheck<cr>", {desc="link note"})
 vim.keymap.set( "n", "gf", function()
     if require('obsidian').util.cursor_on_markdown_link() then
       return "<cmd>ObsidianFollowLink<CR>"
--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -73,6 +73,9 @@ return require('packer').startup(function(use)
     use('folke/which-key.nvim')
 
     -- Obsidian
-    use('epwalsh/obsidian.nvim')
+    -- use('epwalsh/obsidian.nvim')
+    
+    -- Markdown plugin (alt to obsidian)
+    use('jakewvincent/mkdnflow.nvim')
 
 end)
