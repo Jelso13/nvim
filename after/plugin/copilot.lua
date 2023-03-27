0cf05f93f13a38597568b5d7792a74f53f9947b5
Jelso13 <jameselson19@gmail.com>
2023-03-27 10:52:33 +0100

    Added some minimal configuration for copilot

---
+++ after/plugin/copilot.lua
@@ -0,0 +1,15 @@
+-- Setup copilot
+-- local status_ok, copilot = pcall(require, "copilot")
+-- if not status_ok then
+--     return
+-- end
+
+
+vim.keymap.set('n', '<leader>cpe', '<cmd>Copilot enable<cr>', {desc="Enable copilot"})
+vim.keymap.set('n', '<leader>cpd', '<cmd>Copilot disable<cr>', {desc="Disable copilot"})
+vim.keymap.set('n', '<leader>cps', '<cmd>Copilot status<cr>', {desc="Copilot status"})
+vim.keymap.set('n', '<leader>cpp', '<cmd>Copilot panel<cr>', {desc="Copilot panel"})
+
+
+
+
