f08a51c3fa3597ea6132d1b12804d5f00e067b64
James Elson <jameselson19@gmail.com>
2023-06-24 19:27:49 +0100

    fixed issue with vimtex and zathura

--- after/ftplugin/tex/vimtex.lua
+++ after/ftplugin/tex/vimtex.lua
@@ -1,6 +1,7 @@
 vim.cmd([[
     let g:tex_flavor='latex'
     let g:vimtex_view_method='zathura'
+    let g:vimtex_view_general_viewer = 'zathura'
     let g:vimtex_mappings_enabled = 0
     let g:vimtex_imaps_enabled = 0
     let g:vimtex_delim_toggle_mod_list = [
