c7ac1064cf5c8f36a6643d6b6d44018894524e82
James Elson <jameselson19@gmail.com>
2024-09-18 17:05:59 +0100

    feat: Added meta snippets for making snippets

--- lua/snippets/langs/lua.lua
+++ lua/snippets/langs/lua.lua
@@ -7,6 +7,7 @@ if not status_ok then
 end
 -- abbreviations used
 local s = ls.snippet
+local c = ls.choice_node
 local sn = ls.snippet_node
 local t = ls.text_node
 local i = ls.insert_node
@@ -17,43 +18,70 @@ local fmta = require("luasnip.extras.fmt").fmta
 local rep = require("luasnip.extras").rep
 local ls = require("luasnip")
 
-ls.add_snippets("lua", {
-    s("trig_lua", t("loaded from lua file!!")),
-    -- s("req", fmt([[local {} = require "{}"]],
-    --     {f(function(import_name)
-    --         return import_name[1]
-    --     end, { 1 }), i(1) })),
 
-    -- fill in require statement and assign local to last .x part
-    s(
-        "req",
-        fmt([[local {} = require "{}"]], {
-            f(function(import_name)
-                local parts = vim.split(import_name[1][1], ".", true)
-                return parts[#parts] or ""
-            end, { 1 }),
+--    s({trig = "^(.*)hashbox", 
+--    dscr = "Hash box that accounts for comments",
+--    docstring = [[
+--          ######
+--          # <> #
+--          ######
+--          ]],
+--    wordTrig = false, regTrig = true},
+--      fmta([[
+--         <c>#<a>#
+--         <c># <> #
+--         <c>#<a>#
+--        ]],
+--        {
+--          a = f(function(args)
+--            local width = #args[1][1] -- Get the length of the content
+--            return string.rep("#", width + 2) -- Generate a line of dashes with padding
+--          end, {1}), -- Top border
+--          c = f( function(_, snip) return snip.captures[1] end ),
+--          d(1, helpers.get_visual),
+--        }
+--      )
+--    ),
+--
+--
+--     -- Snippet for section
+--     s({ trig = "sec", descr = "LaTeX section" }, {
+--         t("\\section{"), i(1), t("}"),
+--     }),
+
+local meta = {
+    s({trig = "snip", dscr = "Create a box (can also visually wrap)",
+        docstring = [[
+          snippet
+          ]]
+    },
+        fmta(
+          [[
+        s({trig="<>", dscr="<>", 
+            docstring=<>,
+            wordTrig=<>,
+            regTrig=<>
+            }, <>
+        )
+          ]],
+          {
+            -- can set repeats by assigning name
             i(1),
-        })
-    ),
+            i(2),
+            c(3, {
+                fmta('"<>"',i(1)),
+                fmta("[[<>]]",i(1)),
+            }),
+            c(4, {t("false"),t("true")}),
+            c(5, {t("false"),t("true")}),
+            c(6, {
+                fmta('{<>}',i(1)),
+                fmta("fmta([[<>]])",i(1)),
+            }),
+          }
+        )
+      ),
 
-    -- s({
-    --     trig = "snippet",
-    --     desc = "snippet for creating snippets",
-    -- }, {
-    --     fmt([[
-    -- s({
-    --     trig = "{}",
-    --     desc = "{}",
-    -- }, {
-    --     {}
-    -- }
-    --     ]], {
-    --         f(function(trig)
-    --             local parts = vim.split(import_name[1][1], ".", true)
-    --             return parts[#parts] or ""
-    --         end, { 1 }),
-    --         i(1),
-    --     }),
-    -- }),
-})
-local builtin = require("telescope.builtin")
+}
+
+ls.add_snippets("lua", meta)
