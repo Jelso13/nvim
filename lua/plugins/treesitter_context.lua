a46062f189ec6071d62c02cebcc227f82fb4296a
James Elson <jameselson19@gmail.com>
2025-05-08 16:54:38 +0100

    fix: few small changes for treesitter context

--- lua/plugins/treesitter_context.lua
+++ lua/plugins/treesitter_context.lua
@@ -13,16 +13,19 @@ return {
             mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
             -- Separator between context and content. Should be a single character string, like '-'.
             -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+            -- separator = nil,
             separator = nil,
             zindex = 20, -- The Z-index of the context window
             on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
         },
         config = function()
             -- set up keybindings for treesitter context commands
-
             vim.keymap.set("n", "<leader>tc", function()
                 require("treesitter-context").toggle()
             end, { desc = "Toggle Treesitter Context" })
+            vim.keymap.set("n", "[c", function()
+              require("treesitter-context").go_to_context(vim.v.count1)
+            end, { silent = true, desc="Jump up to context" })
         end,
     },
 }
