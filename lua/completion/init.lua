97c9328938c13408da78ca36036d0c06383216b4
James Elson <jameselson19@gmail.com>
2026-04-10 16:47:32 +0100

    fix: can link obsidian notes with [[ now

--- lua/completion/init.lua
+++ lua/completion/init.lua
@@ -1,92 +1,121 @@
-
 return {
     "saghen/blink.cmp",
     event = 'VimEnter',
-    -- optional: provides snippets for the snippet source
     dependencies = { "L3MON4D3/LuaSnip", "rafamadriz/friendly-snippets" },
-    -- use a release tag to download pre-built binaries
     version = "1.*",
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
-
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
+    
     opts = {
-        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-        -- 'super-tab' for mappings similar to vscode (tab to accept)
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
-        -- All presets have the following mappings:
-        -- C-space: Open menu or open docs if already open
-        -- C-n/C-p or Up/Down: Select next/previous item
-        -- C-e: Hide menu
-        -- C-k: Toggle signature help (if signature.enabled = true)
-        --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
         keymap = { preset = "default" },
 
         appearance = {
-            -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-            -- Adjusts spacing to ensure icons are aligned
             nerd_font_variant = "mono",
         },
 
-        -- (Default) Only show the documentation popup when manually triggered
         completion = { documentation = { auto_show = false } },
 
-        -- -- Default list of enabled providers defined so that you can extend it
-        -- -- elsewhere in your config, without redefining it, due to `opts_extend`
-        -- sources = {
-        --     default = { "lsp", "path", "snippets", "buffer" },
-        -- },
-
-        -- Default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, due to `opts_extend`
-        --
         sources = {
-            default = { "obsidian", "obsidian_new", "obsidian_tags", "lsp", "path", "snippets", "buffer" },
-
-            providers = {
-                obsidian = {
-                    name = "obsidian",
-                    module = "obsidian.completion.sources.blink.refs",
-                    score_offset = 100, -- Boost priority
-                    enabled = function() -- only enable if in vault
-                        return vim.api.nvim_buf_get_name(0):find("/Vault") ~= nil
-                    end,
-                },
-                obsidian_new = {
-                    name = "obsidian_new",
-                    module = "obsidian.completion.sources.blink.new",
-                    score_offset = 99, -- Boost priority
-                    enabled = function() -- only enable if in vault
-                        return vim.api.nvim_buf_get_name(0):find("/Vault") ~= nil
-                    end,
-                },
-                obsidian_tags = {
-                    name = "obsidian_tags",
-                    module = "obsidian.completion.sources.blink.tags",
-                    score_offset = 100, -- Boost priority
-                    enabled = function() -- only enable if in vault
-                        return vim.api.nvim_buf_get_name(0):find("/Vault") ~= nil
-                    end,
-                },
-            },
+            -- ONLY use standard defaults here. 
+            -- Obsidian will silently inject itself into this list on startup.
+            default = { "lsp", "path", "snippets", "buffer" },
         },
 
         snippets = { preset = 'luasnip' },
-
-        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-        --
-        -- See the fuzzy documentation for more information
         fuzzy = { implementation = "prefer_rust_with_warning" },
-        -- signature = { enabled = true },
     },
     opts_extend = { "sources.default" },
 }
+
+
+
+-- return {
+--     "saghen/blink.cmp",
+--     event = 'VimEnter',
+--     -- optional: provides snippets for the snippet source
+--     dependencies = { "L3MON4D3/LuaSnip", "rafamadriz/friendly-snippets" },
+--     -- use a release tag to download pre-built binaries
+--     version = "1.*",
+--     -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+--     -- build = 'cargo build --release',
+--     -- If you use nix, you can build from source using latest nightly rust with:
+--     -- build = 'nix run .#build-plugin',
+-- 
+--     ---@module 'blink.cmp'
+--     ---@type blink.cmp.Config
+--     opts = {
+--         -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+--         -- 'super-tab' for mappings similar to vscode (tab to accept)
+--         -- 'enter' for enter to accept
+--         -- 'none' for no mappings
+--         --
+--         -- All presets have the following mappings:
+--         -- C-space: Open menu or open docs if already open
+--         -- C-n/C-p or Up/Down: Select next/previous item
+--         -- C-e: Hide menu
+--         -- C-k: Toggle signature help (if signature.enabled = true)
+--         --
+--         -- See :h blink-cmp-config-keymap for defining your own keymap
+--         keymap = { preset = "default" },
+-- 
+--         appearance = {
+--             -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+--             -- Adjusts spacing to ensure icons are aligned
+--             nerd_font_variant = "mono",
+--         },
+-- 
+--         -- (Default) Only show the documentation popup when manually triggered
+--         completion = { documentation = { auto_show = false } },
+-- 
+--         -- -- Default list of enabled providers defined so that you can extend it
+--         -- -- elsewhere in your config, without redefining it, due to `opts_extend`
+--         -- sources = {
+--         --     default = { "lsp", "path", "snippets", "buffer" },
+--         -- },
+-- 
+--         -- Default list of enabled providers defined so that you can extend it
+--         -- elsewhere in your config, without redefining it, due to `opts_extend`
+--         --
+--         sources = {
+--             -- default = { "obsidian", "obsidian_new", "obsidian_tags", "lsp", "path", "snippets", "buffer" },
+--             default = { "lsp", "path", "snippets", "buffer" },
+-- 
+--             -- providers = {
+--             --     obsidian = {
+--             --         name = "obsidian",
+--             --         module = "obsidian.completion.sources.blink.refs",
+--             --         score_offset = 100, -- Boost priority
+--             --         enabled = function() -- only enable if in vault
+--             --             return vim.api.nvim_buf_get_name(0):find("/Vault") ~= nil
+--             --         end,
+--             --     },
+--             --     obsidian_new = {
+--             --         name = "obsidian_new",
+--             --         module = "obsidian.completion.sources.blink.new",
+--             --         score_offset = 99, -- Boost priority
+--             --         enabled = function() -- only enable if in vault
+--             --             return vim.api.nvim_buf_get_name(0):find("/Vault") ~= nil
+--             --         end,
+--             --     },
+--             --     obsidian_tags = {
+--             --         name = "obsidian_tags",
+--             --         module = "obsidian.completion.sources.blink.tags",
+--             --         score_offset = 100, -- Boost priority
+--             --         enabled = function() -- only enable if in vault
+--             --             return vim.api.nvim_buf_get_name(0):find("/Vault") ~= nil
+--             --         end,
+--             --     },
+--             -- },
+--         },
+-- 
+--         snippets = { preset = 'luasnip' },
+-- 
+--         -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+--         -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+--         -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+--         --
+--         -- See the fuzzy documentation for more information
+--         fuzzy = { implementation = "prefer_rust_with_warning" },
+--         -- signature = { enabled = true },
+--     },
+--     opts_extend = { "sources.default" },
+-- }
 
--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,127 +1,31 @@
--- make it so that it is only loaded when the file is within ~/Vault
+-- 1. Custom Note ID Function
+local function custom_note_id_func(title)
+    if title ~= nil then
+        title = title:gsub("%.md$", "")
+        return title:gsub(" ", "-"):gsub("[^A-Za-z0-9-/]", ""):lower()
+    else
+        return tostring(os.time())
+    end
+end
 
---[[
--- /Sources template:
---        
---        ---
---        tags: [source]
---        ---
---        # Title of the Source
---        
---        **Author/Year:** **Local File:** [[Name of PDF in Attachments]]
---
---        ## One-Sentence Summary
---        >
---
---        ## Notes & Extractions
---
---
---]
-
--- return {
---     "epwalsh/obsidian.nvim",
---     version = "*",
---     lazy = true,
---     event = {
---         "BufReadPre " .. vim.fn.expand("~") .. "/Vault/**/*.md",
---         "BufNewFile " .. vim.fn.expand("~") .. "/Vault/**/*.md",
---     },
---     dependencies = {
---         "nvim-lua/plenary.nvim",
---         "saghen/blink.cmp",
---     },
---     opts = {
---         workspaces = { { name = "Vault", path = "~/Vault", }, },
---         completion = {
---             nvim_cmp = false,
---             blink = true,
---         },
---         -- Fixes legacy commands warning
---         legacy_commands = false,
---
---         -- Protocol: Enforce standard markdown links
---         preferred_link_style = "markdown",
---
---         -- Protocol: The Scratchpad folder
---         daily_notes = {
---             folder = "Scratchpad",
---             date_format = "%Y-%m-%d",
---             alias_format = "%Y-%m-%d",
---             default_tags = { "daily" },
---         },
---
---         -- Protocol: Image routing
---         attachments = {
---             img_folder = "Attachments",
---         },
---
---         -- Fixes frontmatter warning & enforces snippet protocol
---         frontmatter = {
---             enabled = false,
---         },
---
---         -- Protocol: Strict kebab-case filenames
---         note_id_func = function(title)
---             if title ~= nil then
---                 return title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
---             else
---                 return tostring(os.time())
---             end
---         end,
---
---         -- Fixes checkbox ordering warning
---         checkbox = {
---             enabled = true,
---             create_new = true,
---             order = { " ", "x", "~", "!", ">" },
---         },
---
---         -- UI Rendering
---         ui = {
---             enable = true,
---             update_debounce = 200,
---             checkboxes = {
---                 [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
---                 ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
---                 ["!"] = { char = "", hl_group = "ObsidianImportant" },
---                 [">"] = { char = "", hl_group = "ObsidianRightArrow" },
---                 ["x"] = { char = "", hl_group = "ObsidianDone" },
---             },
---         },
---
---         -- OS Integration
---         follow_url_func = function(url)
---             vim.fn.jobstart({ "xdg-open", url })
---         end,
---         follow_img_func = function(url)
---             vim.fn.jobstart({ "xdg-open", url })
---         end,
---     },
---
---     -- Fixes the mappings warning by using native Neovim keymaps
---     config = function(_, opts)
---         require("obsidian").setup(opts)
---
---         -- Standard vim.keymap.set commands using the modern space-separated syntax
---         vim.keymap.set("n", "<leader>od", "<cmd>Obsidian today<cr>", { desc = "[O]bsidian [D]aily Note" })
---         vim.keymap.set("n", "<leader>on", "<cmd>Obsidian new<cr>", { desc = "[O]bsidian [N]ew Note" })
---         vim.keymap.set("v", "<leader>ol", "<cmd>Obsidian link_new<cr>", { desc = "[O]bsidian [L]ink visual selection" })
---         vim.keymap.set("n", "<leader>ob", "<cmd>Obsidian backlinks<cr>", { desc = "[O]bsidian [B]acklinks" })
---         vim.keymap.set("n", "<leader>oo", "<cmd>Obsidian open<cr>", { desc = "[O]bsidian [O]pen in GUI" })
---         vim.keymap.set("n", "<leader>os", "<cmd>Obsidian search<cr>", { desc = "[O]bsidian [S]earch" })
---     end,
--- }
-
-
+-- 2. Custom Note Path Function
+local function custom_note_path_func(spec)
+    local path
+    local id_str = tostring(spec.id)
+    if id_str:match("/") then
+        local vault_root = require("obsidian").get_client().dir
+        path = vault_root / id_str
+    else
+        path = spec.dir / id_str
+    end
+    return path:with_suffix(".md", true)
+end
 
 return {
     "obsidian-nvim/obsidian.nvim",
     version = "*", 
-    lazy = true,
-    event = {
-        "BufReadPre " .. vim.fn.expand("~") .. "/Vault/**/*.md",
-        "BufNewFile " .. vim.fn.expand("~") .. "/Vault/**/*.md",
-    },
+    -- THE FIX: Load on startup to synchronize with blink.cmp
+    lazy = false, 
     dependencies = {
         "nvim-lua/plenary.nvim",
         "saghen/blink.cmp", 
@@ -134,41 +38,30 @@ return {
             pattern = vim.fn.expand("~") .. "/Vault/**/*.md",
             callback = function(ev)
                 local bufnr = ev.buf
-
                 local function map(mode, key, cmd, desc)
                     vim.keymap.set(mode, key, cmd, { noremap = true, silent = true, buffer = bufnr, desc = desc })
                 end
 
-                -- 1. Workspace & Navigation 
+                -- Bindings 
                 map("n", "<leader>sf", "<cmd>Obsidian quick_switch<CR>", "Search Obsidian vault")
                 map("n", "<leader>oo", "<cmd>Obsidian open<CR>", "Open in Obsidian App")
                 map("n", "<localleader>o", "<cmd>Obsidian open<CR>", "Open in Obsidian App")
-
-                -- 2. Creation & Templates 
                 map("n", "<leader>on", "<cmd>Obsidian new<CR>", "Create new note")
                 map("n", "<leader>ot", "<cmd>Obsidian template<CR>", "Insert template")
                 map("n", "<leader>oT", "<cmd>Obsidian new_from_template<CR>", "New note from template")
-
-                -- 3. Daily Notes 
                 map("n", "<leader>od", "<cmd>Obsidian today<CR>", "Today's daily note")
                 map("n", "<localleader>d", "<cmd>Obsidian today<CR>", "Today's daily note")
                 map("n", "<leader>oy", "<cmd>Obsidian yesterday<CR>", "Yesterday's daily note")
                 map("n", "<leader>om", "<cmd>Obsidian tomorrow<CR>", "Tomorrow's daily note")
                 map("n", "<leader>oD", "<cmd>Obsidian dailies<CR>", "List daily notes")
                 map("n", "<localleader>D", "<cmd>Obsidian dailies<CR>", "List daily notes")
-
-                -- 4. Note Structure & Linking 
                 map("n", "<leader>ob", "<cmd>Obsidian backlinks<CR>", "Show note backlinks")
                 map("n", "<leader>oL", "<cmd>Obsidian links<CR>", "Show links in note")
                 map("n", "<leader>og", "<cmd>Obsidian tags<CR>", "Search tags")
                 map("n", "<leader>oc", "<cmd>Obsidian toc<CR>", "Table of contents")
-
-                -- 5. Note Editing
                 map("n", "<leader>or", "<cmd>Obsidian rename<CR>", "Rename note")
                 map("n", "<leader>ox", "<cmd>Obsidian toggle_checkbox<CR>", "Toggle checkbox")
                 map("n", "<leader>op", "<cmd>Obsidian paste_img<CR>", "Paste image")
-
-                -- 6. Visual Mode Bindings 
                 map("v", "<leader>ol", "<cmd>Obsidian link<CR>", "Link visual selection")
                 map("v", "<leader>on", "<cmd>Obsidian link_new<CR>", "Link visual to new note")
                 map("v", "<leader>oe", "<cmd>Obsidian extract_note<CR>", "Extract text to new note")
@@ -181,10 +74,8 @@ return {
             { name = "Vault", path = "~/Vault" },
         },
         
-        -- Prevent the plugin from injecting default frontmatter before templates load
         disable_frontmatter = true, 
-        
-        completion = { blink = false }, 
+        completion = { nvim_cmp = false, blink = true }, 
         attachments = {
             folder = "Attachments",
             img_folder = "Attachments",
@@ -205,27 +96,22 @@ return {
         checkbox = {
             create_new = false,
         },
-        note_id_func = function(title)
-            if title ~= nil then
-                return title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-            else
-                return tostring(os.time())
-            end
-        end,
         daily_notes = {
             folder = "Scratchpad",
             date_format = "%Y-%m-%d",
             alias_format = "%Y-%m-%d",
             default_tags = { "daily" },
         },
+        
+        note_id_func = custom_note_id_func,
+        note_path_func = custom_note_path_func,
+
         callbacks = {
             enter_note = function(note)
                 local bufnr = vim.api.nvim_get_current_buf()
-                -- Since frontmatter is disabled, brand new notes will have exactly 1 empty line
                 if vim.api.nvim_buf_line_count(bufnr) > 1 then
                     return
                 end
-                vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {})
 
                 local path = tostring(note.path):lower() 
 
